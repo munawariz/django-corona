831e7deb60fdfb3d8d209c9f060dc01aded9d2ff
harizMunawar <munawarhariz@gmail.com>
2020-07-13 18:17:27 +0700

    Using tailwind css also changing api to mathdroid

--- pages/views.py
+++ pages/views.py
@@ -2,55 +2,71 @@ from django.shortcuts import render
 import requests
 
 def index(request):
+    image = requests.get('https://covid19.mathdro.id/api/og')
     context = {
         'page_title': 'index',
+        'image': image.url,
     }
     return render(request, 'index.html', context)
 
 def global_case(request):
-    positif = requests.get('https://api.kawalcorona.com/positif').json()
-    sembuh = requests.get('https://api.kawalcorona.com/sembuh').json()
-    meninggal = requests.get('https://api.kawalcorona.com/meninggal').json()
+    data = requests.get('https://covid19.mathdro.id/api/').json()
     context = {
         'page_title': 'Global Case',
-        'positif': positif['value'],
-        'sembuh': sembuh['value'],
-        'meninggal': meninggal['value'],
+        'positif': data['confirmed']['value'],
+        'sembuh': data['recovered']['value'],
+        'meninggal': data['deaths']['value'],
     }
-    return render(request, 'index.html', context)
+    return render(request, 'case.html', context)
 
 def list_country(request):
-    data = requests.get('https://api.kawalcorona.com/').json()
+    data = requests.get('https://covid19.mathdro.id/api/countries/').json()   
+    data = data['countries']
     country_list = []
+    link = []
+    views_data = []
+    
     for data in data:
-        country = data['attributes']['Country_Region']
-        country_list.append(country)
+        country_list.append(data['name'].lower())
+        if ' ' in data['name']:
+            link_name = data['name'].replace(' ', '')
+            link.append(link_name.lower())
+        else:
+            link_name = data['name']
+            link.append(data['name'].lower())
+        views_data.append({'name':data['name'].lower(), 'link':link_name.lower()})
+        
     context = {
         'page_title' : 'List Country',
         'country_list' : country_list,
+        'links' : link,
+        'data' : views_data,
     }
-    return render(request, 'index.html', context)
+    return render(request, 'country_list.html', context)
 
 def country(request, country_requested):
-    data = requests.get('https://api.kawalcorona.com/').json()
+    if '%20' in country_requested:
+        country_requested.replace('%20', ' ')
+    data = requests.get(f'https://covid19.mathdro.id/api/countries/{country_requested}').json()
+    print(data)
 
-    for data in data:
-        country = data['attributes']['Country_Region']
-        if country_requested.lower() in country.lower():
-            positif = data['attributes']['Confirmed']
-            sembuh = data['attributes']['Recovered']
-            meninggal = data['attributes']['Deaths']
-            context = {
-                'page_title' : 'Country Case',
-                'country' : country_requested,
-                'positif': positif,
-                'sembuh': sembuh,
-                'meninggal': meninggal,
-            }
-            break
-        else:
-            context = {
-                'page_title' : 'Country Case',
-                'country' : country_requested,
-            }
-    return render(request, 'index.html', context)
+    # for data in data:
+        # country = data['attributes']['Country_Region']
+        # if country_requested.lower() in country.lower():
+    positif = data['confirmed']['value']
+    sembuh = data['recovered']['value']
+    meninggal = data['deaths']['value']
+    context = {
+        'page_title' : 'Country Case',
+        'country' : country_requested,
+        'positif': positif,
+        'sembuh': sembuh,
+        'meninggal': meninggal,
+    }
+            # break
+        # else:
+        #     context = {
+        #         'page_title' : 'Country Case',
+        #         'country' : country_requested,
+        #     }
+    return render(request, 'case.html', context)
