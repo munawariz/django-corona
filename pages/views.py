7cbc7c438e7a4ed65ebd648d2c015b774d3bda35
harizMunawar <munawarhariz@gmail.com>
2020-07-25 01:19:32 +0700

    Adding world case visualization with scattergeo

--- pages/views.py
+++ pages/views.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.io as pio
-pio.templates.default = "simple_white"
+pio.templates.default = "plotly_white"
 import plotly.express as px
 import plotly.offline as opy
 
@@ -13,19 +13,16 @@ import plotly.offline as opy
 def index(request):
     try:
         data = requests.get('https://corona-api.com/timeline').json()
-        last_data = data['data'][0]
-        prev_data = []
-
-        for x in range(3):
-            prev_data.append(data['data'][x+1])        
-
+        last_data = data['data'][0]                 
+        
+        div = get_scattergeo()
         context = {
             'data': last_data,
-            'prev_data': prev_data,
+            'graph': div,
         }
         return render(request, 'page/global.html', context)
     except Exception as e:
-        print(e)
+        print(f'the error is {e}')
 
 def list_country(request):
     try:
@@ -63,25 +60,20 @@ def list_country(request):
 def country(request, *, country_requested):
     try:
         data = requests.get(f'https://corona-api.com/countries/{country_requested}').json()
-        last_data = data['data']
-        country_name = last_data['name']     
+        last_data = data['data']    
         timeline_data = []
         
         try:
             for x in range(100):                       
                 timeline = data['data']['timeline'][x+1]   
                 if timeline:
-                    timeline_data.append([str(timeline['date']), int(timeline['confirmed']), int(timeline['recovered']), int(timeline['deaths'])])
-            
-            df = pd.DataFrame(timeline_data, columns=['Date', 'Confirmed', 'Recovered', 'Deaths'])
-            df = df[::-1]
-            df_long = pd.melt(df, id_vars=['Date'], value_vars=['Confirmed', 'Recovered', 'Deaths'])            
-            country_fig = px.line(df_long, x="Date", y="value", color='variable')
-            country_fig.update_layout(
-                autosize = True,
-                margin=dict(l=0, r=0, t=10, b=0),
-            )
-            div = opy.plot(country_fig, output_type='div', auto_open=False)
+                    timeline_data.append([
+                        str(timeline['date']), 
+                        int(timeline['confirmed']), 
+                        int(timeline['recovered']), 
+                        int(timeline['deaths'])]
+                        )
+            div = get_graph(timeline_data)
         except IndexError:
             div = None
         
@@ -103,3 +95,52 @@ def error_404(request, exception):
 
 def error_500(request):
     return render(request, 'page/error404.html')
+
+def get_graph(dataframe):
+    df = pd.DataFrame(dataframe, columns=['Date', 'Confirmed', 'Recovered', 'Deaths'])
+    df = df[::-1]
+    df_long = pd.melt(df, id_vars=['Date'], value_vars=['Confirmed', 'Recovered', 'Deaths'])            
+    country_fig = px.line(df_long, x="Date", y="value", color='variable', labels={'value': 'Case', 'variable': 'Type'})
+    country_fig.update_layout(
+        autosize = True,
+        margin=dict(l=0, r=0, t=10, b=0),
+    )
+    return opy.plot(country_fig, output_type='div', auto_open=False)
+
+def get_scattergeo():
+    data = requests.get('https://corona-api.com/countries').json()
+    data = data['data']
+    country_data = []
+
+    for data in data:
+        case = data['latest_data']
+        country_data.append([
+            str(data['name']),
+            int(case['confirmed']),
+            int(case['recovered']),
+            int(case['deaths']),]
+            )
+
+    df = pd.DataFrame(country_data, columns=['Name', 'Confirmed', 'Recovered', 'Deaths'])
+
+    map_fig = px.scatter_geo(
+        data_frame = df,
+        locations = df['Name'],
+        locationmode = 'country names',
+        color = df['Name'],
+        hover_name = df['Name'],
+        hover_data = [
+            df['Confirmed'],
+            df['Recovered'],
+            df['Deaths'],
+        ],
+        size = df['Confirmed'],
+        labels = {'Name': 'Country Name'},
+    )
+
+    map_fig.update_layout(
+        autosize = True,
+        margin=dict(l=0, r=0, t=10, b=0),
+    )
+
+    return opy.plot(map_fig, output_type='div', auto_open=False)
